03b259253d1c9580ca736bbc302f1e7c2e19880a
Mathias Rogey <mathias.rogey@eduvaud.ch>
2020-04-25 10:29:42 +0200

    Version 2.5 : Global Optimisation + visual improvment

--- Echiquier/Form1.cs
+++ Echiquier/Form1.cs
@@ -26,9 +26,9 @@ namespace Echiquier
             g_boolTabCheckCavalierFini = new bool[g_byteNbrCases * g_byteNbrCases];
             g_boolTabJoueur = new bool[g_byteNbrCases, g_byteNbrCases];
 
-            for (int y = 0; y < g_byteNbrCases; y++)
+            for (short y = 0; y < g_byteNbrCases; y++)
             {
-                for (int x = 0; x < g_byteNbrCases; x++)
+                for (short x = 0; x < g_byteNbrCases; x++)
                 {
                     // initialise le tablea du joueur avec des false, donc il n'est pas passé par la
                     g_boolTabJoueur[x, y] = false;
@@ -57,6 +57,15 @@ namespace Echiquier
 
                     // premier event handler qui va tout initaliser
                     picBox.Click += new EventHandler(PosCavalierViaClick);
+
+                    // permet d'afficher si l'user pointe une case légale
+                    picBox.MouseEnter += new EventHandler(CheckPosCavalierViaSouris);
+
+                    // reset la case quand l'user a fini de la pointer
+                    picBox.MouseLeave += new EventHandler(ResetCaseSourisLeave);
+
+                    // montre sur quel case l'user pointe quand il entre dedan avec la souris
+                    picBox.MouseEnter += new EventHandler(InfoCase);
 
                     // met l'image par raport a la taille de la picture box
                     picBox.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -92,8 +101,19 @@ namespace Echiquier
             // permet de changer le nom du picBox en position XY dans le cavalier
             byte[] tab_bytePosXYViaNom = new byte[] { (byte)((byte)Convert.ToChar(tab_strStringNamePicBox[0]) - 65), Convert.ToByte(tab_strStringNamePicBox[1]) };
 
+            // permet de reset la case quand l'user a cliqué dessus car : on entre dans la case et n'est jamais reset avec la méthode " ResetCaseSourisLeave "
+            // si false, user jamais passé par la \|/ si true, user déjà passé par la
+            if (g_boolTabJoueur[CtrlSender.Location.X / CtrlSender.Width, CtrlSender.Location.Y / CtrlSender.Width] == false)
+            {
+                CtrlSender.BackColor = ((CtrlSender.Location.X / CtrlSender.Width) + (CtrlSender.Location.Y / CtrlSender.Width)) % 2 == 0 ? Color.White : Color.Orange;
+            }
+            else
+            {
+                CtrlSender.BackColor = ((CtrlSender.Location.X / CtrlSender.Width) + (CtrlSender.Location.Y / CtrlSender.Width)) % 2 == 0 ? Color.Green : Color.DarkGreen;
+            }
+
             // check si le joueur a cliqué sur une position valide
-            if (CheckPos(CtrlSender.Location.X, CtrlSender.Location.Y, CtrlSender.Width))
+            if (CheckPos((short)CtrlSender.Location.X, (short)CtrlSender.Location.Y, (short)CtrlSender.Width))
             {
                 // set dans la position XY du joueur que il est allé sur cette case
                 g_boolTabJoueur[tab_bytePosXYViaNom[0], tab_bytePosXYViaNom[1]] = true;
@@ -135,28 +155,28 @@ namespace Echiquier
             }
         }
 
-        private bool CheckPos(int intX, int intY, int intWidth)
+        private bool CheckPos(short shortX, short shortY, short shortWidth)
         {
             // variables
-            int intTailleCase = panEchiquier.Width / g_byteNbrCases;
-            int[] tabPosXY = new int[] { intX, intY };
+            short shortTailleCase = (short)(panEchiquier.Width / g_byteNbrCases);
+            short[] tabPosXY = new short[] { shortX, shortY };
 
             // ici le but est de voir par rapport a la case cliqué si le cavalier se trouve dessus
             // les commentaires montre ou le joueur doit cliquer pour que la condition soit true
             // la condition check par rapport a la case cliqué
             // mon point d'origine est en haut a gauche, donc le 0,0
             // check a chaque fois si le cavalier est dans une position légal
-            if (intX - intTailleCase * 1 == g_intTabPosBufferXY[0] && intY + intTailleCase * 2 == g_intTabPosBufferXY[1] ||
-                intX - intTailleCase * 2 == g_intTabPosBufferXY[0] && intY + intTailleCase * 1 == g_intTabPosBufferXY[1] ||
-                intX - intTailleCase * 2 == g_intTabPosBufferXY[0] && intY - intTailleCase * 1 == g_intTabPosBufferXY[1] ||
-                intX - intTailleCase * 1 == g_intTabPosBufferXY[0] && intY - intTailleCase * 2 == g_intTabPosBufferXY[1] ||
+            if (shortX - shortTailleCase * 1 == g_shortTabPosBufferXY[0] && shortY + shortTailleCase * 2 == g_shortTabPosBufferXY[1] ||
+                shortX - shortTailleCase * 2 == g_shortTabPosBufferXY[0] && shortY + shortTailleCase * 1 == g_shortTabPosBufferXY[1] ||
+                shortX - shortTailleCase * 2 == g_shortTabPosBufferXY[0] && shortY - shortTailleCase * 1 == g_shortTabPosBufferXY[1] ||
+                shortX - shortTailleCase * 1 == g_shortTabPosBufferXY[0] && shortY - shortTailleCase * 2 == g_shortTabPosBufferXY[1] ||
                 // fin droite
-                intX + intTailleCase * 1 == g_intTabPosBufferXY[0] && intY - intTailleCase * 2 == g_intTabPosBufferXY[1] ||
-                intX + intTailleCase * 2 == g_intTabPosBufferXY[0] && intY - intTailleCase * 1 == g_intTabPosBufferXY[1] ||
-                intX + intTailleCase * 2 == g_intTabPosBufferXY[0] && intY + intTailleCase * 1 == g_intTabPosBufferXY[1] ||
-                intX + intTailleCase * 1 == g_intTabPosBufferXY[0] && intY + intTailleCase * 2 == g_intTabPosBufferXY[1] )
+                shortX + shortTailleCase * 1 == g_shortTabPosBufferXY[0] && shortY - shortTailleCase * 2 == g_shortTabPosBufferXY[1] ||
+                shortX + shortTailleCase * 2 == g_shortTabPosBufferXY[0] && shortY - shortTailleCase * 1 == g_shortTabPosBufferXY[1] ||
+                shortX + shortTailleCase * 2 == g_shortTabPosBufferXY[0] && shortY + shortTailleCase * 1 == g_shortTabPosBufferXY[1] ||
+                shortX + shortTailleCase * 1 == g_shortTabPosBufferXY[0] && shortY + shortTailleCase * 2 == g_shortTabPosBufferXY[1] )
             {
-                    return ChangementBuffer(intX, intY, intWidth);
+                    return ChangementBuffer(shortX, shortY, shortWidth);
             }
             // si non alors joueur clique sur mauvaise case
             else
@@ -165,14 +185,14 @@ namespace Echiquier
             }
         }
 
-        private bool ChangementBuffer(int intX, int intY, int intWidth)
+        private bool ChangementBuffer(short shortX, short shortY, short shortWidth)
         {
             // permet d'enlever l'image du cavalier de la case précèdente
-            g_ListePicBox[((((g_intTabPosBufferXY[1] / intWidth) + 1) * g_byteNbrCases) - (g_byteNbrCases - ((g_intTabPosBufferXY[0] / intWidth) + 1))) - 1].Image = null;
+            g_ListePicBox[((((g_shortTabPosBufferXY[1] / shortWidth) + 1) * g_byteNbrCases) - (g_byteNbrCases - ((g_shortTabPosBufferXY[0] / shortWidth) + 1))) - 1].Image = null;
 
             // permet de compacter les lignes de codes qui revenait dans la méthode CheckPos
-            g_intTabPosBufferXY[0] = intX;
-            g_intTabPosBufferXY[1] = intY;
+            g_shortTabPosBufferXY[0] = shortX;
+            g_shortTabPosBufferXY[1] = shortY;
 
             // return forcément true que que la methode a été appelé
             return true;
@@ -187,7 +207,7 @@ namespace Echiquier
             }
 
             // reset des variables
-            g_intTabPosBufferXY = new int[g_byteNbrCases * g_byteNbrCases];
+            g_shortTabPosBufferXY = new short[g_byteNbrCases * g_byteNbrCases];
             g_boolTabJoueur = new bool[g_byteNbrCases, g_byteNbrCases];
             g_boolTabCheckCavalierFini = new bool[g_byteNbrCases * g_byteNbrCases];
             g_ListePicBox.Clear();
@@ -242,8 +262,8 @@ namespace Echiquier
             PictureBox CtrlSender = ((PictureBox)sender);
 
             // set dans le buffer la position du click
-            g_intTabPosBufferXY[0] = CtrlSender.Location.X;
-            g_intTabPosBufferXY[1] = CtrlSender.Location.Y;
+            g_shortTabPosBufferXY[0] = (short)CtrlSender.Location.X;
+            g_shortTabPosBufferXY[1] = (short)CtrlSender.Location.Y;
 
             // ckeck quel variation de couleur il doit mettre par rapport a la case ou le joueur a cliqué
             CtrlSender.BackColor = ((CtrlSender.Location.X / CtrlSender.Width) + (CtrlSender.Location.Y / CtrlSender.Width)) % 2 == 0 ? Color.Green : Color.DarkGreen;
@@ -257,9 +277,53 @@ namespace Echiquier
             // permet d'ajouter les event handler check pose et de retirer celui utilisé actuellement
             foreach (PictureBox item in g_ListePicBox)
             {
-                item.Click += new EventHandler(InfoCase);
                 item.Click += new EventHandler(PositionCavalier);
                 item.Click -= PosCavalierViaClick;
+            }
+        }
+
+        private void CheckPosCavalierViaSouris(object sender, EventArgs e)
+        {
+            // variables
+            PictureBox CtrlSender = ((PictureBox)sender);
+            short shortX = (short)CtrlSender.Location.X;
+            short shortY = (short)CtrlSender.Location.Y;
+            short shortTailleCase = (short)CtrlSender.Width;
+
+            // check si la souris est sur une position légal du cavalier, si true la met en vert clair \|/ si false la met en rouge
+            if (shortX - shortTailleCase * 1 == g_shortTabPosBufferXY[0] && shortY + shortTailleCase * 2 == g_shortTabPosBufferXY[1] ||
+                shortX - shortTailleCase * 2 == g_shortTabPosBufferXY[0] && shortY + shortTailleCase * 1 == g_shortTabPosBufferXY[1] ||
+                shortX - shortTailleCase * 2 == g_shortTabPosBufferXY[0] && shortY - shortTailleCase * 1 == g_shortTabPosBufferXY[1] ||
+                shortX - shortTailleCase * 1 == g_shortTabPosBufferXY[0] && shortY - shortTailleCase * 2 == g_shortTabPosBufferXY[1] ||
+                // fin droite
+                shortX + shortTailleCase * 1 == g_shortTabPosBufferXY[0] && shortY - shortTailleCase * 2 == g_shortTabPosBufferXY[1] ||
+                shortX + shortTailleCase * 2 == g_shortTabPosBufferXY[0] && shortY - shortTailleCase * 1 == g_shortTabPosBufferXY[1] ||
+                shortX + shortTailleCase * 2 == g_shortTabPosBufferXY[0] && shortY + shortTailleCase * 1 == g_shortTabPosBufferXY[1] ||
+                shortX + shortTailleCase * 1 == g_shortTabPosBufferXY[0] && shortY + shortTailleCase * 2 == g_shortTabPosBufferXY[1] ||
+                g_shortTabPosBufferXY[0] == -1 || g_shortTabPosBufferXY[1] == -1 )
+            {
+                CtrlSender.BackColor = Color.LightGreen;       
+            }
+            else
+            {
+                CtrlSender.BackColor = Color.Red;
+            }
+        }
+
+        private void ResetCaseSourisLeave(object sender, EventArgs e)
+        {
+            // variables
+            PictureBox CtrlSender = ((PictureBox)sender);
+
+            // permet de reset la case quand l'user sort de la case
+            // si false, user jamais passé par la \|/ si true, user déjà passé par la
+            if (g_boolTabJoueur[CtrlSender.Location.X / CtrlSender.Width, CtrlSender.Location.Y / CtrlSender.Width] == false)
+            {
+                CtrlSender.BackColor = ((CtrlSender.Location.X / CtrlSender.Width) + (CtrlSender.Location.Y / CtrlSender.Width)) % 2 == 0 ? Color.White : Color.Orange;
+            }
+            else
+            {
+                CtrlSender.BackColor = ((CtrlSender.Location.X / CtrlSender.Width) + (CtrlSender.Location.Y / CtrlSender.Width)) % 2 == 0 ? Color.Green : Color.DarkGreen;
             }
         }
     }
--- Echiquier/Variables.cs
+++ Echiquier/Variables.cs
@@ -3,7 +3,7 @@
     public class Variables
     {
         public static byte g_byteNbrCases = 8;
-        public static int[] g_intTabPosBufferXY = { -1, -1 };
+        public static short[] g_shortTabPosBufferXY = { -1, -1 };
         public static bool[,] g_boolTabJoueur = new bool[g_byteNbrCases, g_byteNbrCases];
         public static bool[] g_boolTabCheckCavalierFini = new bool[g_byteNbrCases * g_byteNbrCases];
     }
